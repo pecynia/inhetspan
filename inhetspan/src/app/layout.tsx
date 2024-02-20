f4d67dc82053fa248ce1f93d494ef00d5e64e1ee
Nicolai Verheul <n.verheul@student.science.ru.nl>
2024-02-20 16:16:36 +0100

    Update language to Dutch in layout.tsx

--- inhetspan/src/app/layout.tsx
+++ inhetspan/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }) {
 
   return (
-    <html lang="en" className={`${inter.className} h-full`}>
+    <html lang="nl" className={`${inter.className} h-full`}>
       <body className='flex min-h-full flex-col font-poppins'>
         <NextAuthProvider>
           <Header />
